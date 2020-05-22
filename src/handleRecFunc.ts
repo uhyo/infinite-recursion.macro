9bb38697a9899fad5ad0bd3db6da88fc548e5a71
uhyo <uhyo@uhy.ooo>
2020-05-22 23:26:21 +0900

    call => yield transform

---
+++ src/handleRecFunc.ts
@@ -0,0 +1,55 @@
+import { NodePath } from "@babel/core";
+import {
+  ArgumentPlaceholder,
+  arrayExpression,
+  BlockStatement,
+  FunctionExpression,
+  Identifier,
+  yieldExpression,
+  JSXNamespacedName,
+} from "@babel/types";
+import { MacroError } from "babel-plugin-macros";
+
+export const handleRecFunc = (
+  funcName: Identifier,
+  funcPath: NodePath<FunctionExpression>
+): boolean => {
+  const blockPath = funcPath.get("body") as NodePath<BlockStatement>;
+  const funcScope = blockPath.scope;
+  const selfBinding = funcScope.getBinding(funcName.name);
+  if (!selfBinding) {
+    return false;
+  }
+
+  // change this to generator function
+  funcPath.node.generator = true;
+
+  for (const path of selfBinding.referencePaths) {
+    // `path` is a self-refernce to the recursive function
+    const { node, parentPath } = path;
+    if (parentPath.isCallExpression() && parentPath.node.callee === node) {
+      // rec(...)
+      const args = parentPath.node.arguments;
+      if (
+        args.some(
+          (arg) =>
+            arg.type === "ArgumentPlaceholder" ||
+            arg.type === "JSXNamespacedName"
+        )
+      ) {
+        throw new MacroError("Unsupported argument type");
+      }
+      type T = typeof args[number];
+      const checkedArgs = args as Array<
+        Exclude<typeof args[number], ArgumentPlaceholder | JSXNamespacedName>
+      >;
+
+      // replace func(...args) with yield [...args]
+      const repl = yieldExpression(arrayExpression(checkedArgs));
+      parentPath.replaceWith(repl);
+      continue;
+    }
+    throw new MacroError(`Unsupported reference to '${funcName.name}`);
+  }
+  return true;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
+import { NodePath } from "@babel/core";
 import { createMacro } from "babel-plugin-macros";
+import { isFunctionExpression, FunctionExpression } from "@babel/types";
+import { handleRecFunc } from "./handleRecFunc";
 
-export = createMacro(({ references, state, babel }) => {});
+export = createMacro(({ references, state, babel }) => {
+  const recFuncReferences = [
+    ...(references.default || []),
+    ...(references.rec || []),
+  ];
+  for (const path of recFuncReferences) {
+    const { node, parentPath } = path;
+    if (parentPath.isCallExpression() && parentPath.node.callee === node) {
+      // rec(...)
+      const { node: parent } = parentPath;
+      const args = parent.arguments;
+      if (args.length === 0) {
+        // invalid: rec()
+        continue;
+      }
+      const firstArg = args[0];
+      if (!isFunctionExpression(firstArg)) {
+        continue;
+      }
+
+      // rec(function () { ... }, ...)
+      if (!firstArg.id) {
+        continue;
+      }
+      if (firstArg.generator || firstArg.async) {
+        continue;
+      }
+      const funcPath = (parentPath.get("arguments.0") as unknown) as NodePath<
+        FunctionExpression
+      >;
+      handleRecFunc(firstArg.id, funcPath);
+    }
+  }
+});
