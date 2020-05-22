1afbda65a477f2d517a70fec23095143458be7ea
uhyo <uhyo@uhy.ooo>
2020-05-23 01:27:08 +0900

    add infinte example

--- example/src/index.js
+++ example/src/index.js
@@ -1,4 +1,10 @@
-import { rec } from "infinite-recursion.macro";
+import { infinite, rec } from "infinite-recursion.macro";
+
+const sum = infinite(function sum(upTo) {
+  return upTo <= 0 ? 0 : upTo + sum(upTo - 1);
+});
+
+console.log(sum(1e6));
 
 const sum1e7Rec = rec(function sum(upTo) {
   return upTo <= 0 ? 0 : upTo + sum(upTo - 1);
